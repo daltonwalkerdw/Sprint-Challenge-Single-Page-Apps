2246774e43ea17622353145eedafad3f06f6eb90
daltonwalkerdw <53591219+daltonwalkerdw@users.noreply.github.com>
2019-11-09 14:00:34 -0500

    ?

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,51 @@
 import React, { useEffect, useState } from "react";
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+import axios from "axios"
+import { Link } from "react-router-dom";
+import CharacterCard from "./CharacterCard"
 
+function CharacterList() {
+  // TODO: Add useState to track data from useEffect
+  const [charactersData, setCharacters] = useState()
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setCharacters(response.data.results);
+        console.log(response)
+
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }, [charactersData]);
+  console.log("character = ", charactersData)
+
+  if (charactersData == []) {
+    return "";
+  } else {
+    return (
+      <section className="character-list">
+        {charactersData.map(cv => {
+          return <CharacterCard datapassed={cv} />;
+        })}
+      </section>
+      // <p>somthing</p>
+    );
+  }
+  //   return (
+  //     // <section className="character-list">
+  //     //   {characters.map( character => {
+
+  //     //       <h1>{character.name}</h1>
+
+  //     //   })}
+  //     // </section>
+  //     <p>renders</p>
+  //   );
 }
+
+export default CharacterList
